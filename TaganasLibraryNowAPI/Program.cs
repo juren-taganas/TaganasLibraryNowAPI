7129c55b340fc83a654c531ae98a704b4d66303c
juren-taganas <smileforward131@gmail.com>
2026-05-09 18:40:25 +0800

    Update Program.cs

--- TaganasLibraryNowAPI/Program.cs
+++ TaganasLibraryNowAPI/Program.cs
@@ -13,13 +13,10 @@ var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"Http://+:{port}");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
